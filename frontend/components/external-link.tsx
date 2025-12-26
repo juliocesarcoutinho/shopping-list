bf16e9a4a20e5f943f49675a29ea5fb50af065b2
Julio Cesar Coutinho <juliocesar.coutinhodev@outlook.com>
2025-12-26 14:54:05 -0300

    Add Prettier configuration and update project files for consistent formatting

--- frontend/components/external-link.tsx
+++ frontend/components/external-link.tsx
@@ -7,10 +7,10 @@ type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: Href & string }
 export function ExternalLink({ href, ...rest }: Props) {
   return (
     <Link
-      target="_blank"
+      target='_blank'
       {...rest}
       href={href}
-      onPress={async (event) => {
+      onPress={async event => {
         if (process.env.EXPO_OS !== 'web') {
           // Prevent the default behavior of linking to the default browser on native.
           event.preventDefault();
